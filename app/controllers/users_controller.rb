52ecaedbac533af53607f742bfd9e95cc865776f
ken-zo555 <ken_zo555@yahoo.co.jp>
2017-10-27 03:00:08 +0000

    user login 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,12 +21,13 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
     end
   end
   
   private
   
   def user_params
-    params.require(:user).permit(:new, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
